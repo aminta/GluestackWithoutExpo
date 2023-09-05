3da170510cd96139a443ced92d1d3606b5606bfc
Davide Lorigliola <davide.lorigliola@beliven.com>
2023-09-05 12:24:27 +0200

    init

---
+++ App.tsx
@@ -0,0 +1,47 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {config, GluestackUIProvider, Text} from '@gluestack-ui/themed';
+export default function App() {
+  return (
+    <GluestackUIProvider config={config.theme}>
+      <Text>Hello World!</Text>
+    </GluestackUIProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
